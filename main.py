403d1b8e07f6d13f03436ed34c3fee4b14ea6b6b
Spotscallab <soccerlegend110@icloud.com>
2020-06-24 23:28:32 +0000

    add a line

---
+++ main.py
@@ -0,0 +1,42 @@
+print("You have found 3 doors: 1, 2, 3")
+firstQuest = input("Which door do you want to walk through? ")
+
+if (firstQuest == "1"):
+    print("Welcome to Door 1")
+    ques = input("What is your favourite sport {soccer, basketball, hockey}: ")
+
+    if (ques == "soccer"):
+      print("You will win a Bughati")
+
+    if (ques == "basketball"):
+      print("You will be faster than the flash")
+
+    if (ques == "hockey"):
+      print("You will live the craziest life ever")
+      
+
+if (firstQuest == "2"):
+    print("Welcome to Door 2")
+    ques = input ("What is your favourite food {pizza, burgers, hotdogs}: ")
+
+    if (ques == "pizza"):
+      print("You will own a private jet")
+
+    if (ques == "burgers"):
+      print("You will be the prime minister of canada")
+
+    if (ques == "hotdogs"):
+      print("You will be friends with Elon Musk")
+
+if (firstQuest == "3"):
+    print("Welcome to Door 3")
+    ques = input ("Are you smart {yes, no, maybe}: ")
+
+    if (ques == "yes"):
+      print("You dress nice")
+
+    if (ques == "no"):
+      print("work harder")
+
+    if (ques == "maybe"):
+      print("You like sky diving")
