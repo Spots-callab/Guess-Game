4cd0082e4cded9992a82838127c7d69ec16b4dc0
Spotscallab <soccerlegend110@icloud.com>
2020-06-25 06:02:41 +0000

    Added a few lines

--- main.py
+++ main.py
@@ -8,12 +8,10 @@ if (firstQuest == "1"):
     if (ques == "soccer"):
       print("You will win a Bughati")
 
+  
     if (ques == "basketball"):
       print("You will be faster than the flash")
 
-    if (ques == "hockey"):
-      print("You will live the craziest life ever")
-      
 
 if (firstQuest == "2"):
     print("Welcome to Door 2")
@@ -40,3 +38,94 @@ if (firstQuest == "3"):
 
     if (ques == "maybe"):
       print("You like sky diving")
+
+print("You have found 3 more doors: 4, 5, 6")
+secondQuest = input("Which door do you want to walk through? ")
+
+if (secondQuest == "4"):
+      print("Welcome to door 4")
+      ques = input("Who is your favourite celebrity {Kendrick Lamar, Adam Sandler, Donald Trump}: ")
+      if (ques == "Kendrick Lamar"):
+        print("You have good taste in music")
+
+      if (ques == "Adam Sandler"):
+       print("You are fun and like to laugh")
+
+      if (ques == "Donald Trump"):
+        print("You are dumb")
+
+if (secondQuest == "5"):
+    print("Welcome to door 5")
+    ques = input("What is your favourite colour{Blue, Red, Green}: ")
+
+    if (ques == "Blue"):
+      print("You are really cool")
+
+    if (ques == "Red"):
+      print("Not a bad choice")
+
+    if (ques == "Green"):
+      print("You are a scycopath")
+
+    if (ques == "hockey"):
+      print("You will live the craziest life ever")
+
+if (secondQuest == "6"):
+      print("Welcome to door 6")
+      ques = input("Which phone do you prefer{iPhone, Samsung, Google}: ")
+    
+      if (ques == "iPhone"):
+        print("You are very clever")
+
+      if (ques == "Samsung"):
+       print("Stupidddd")
+
+      if (ques == "Google"):
+        print("Shake My Head")
+  
+print("You have unlocked a few more doors: 7,8,9")
+thirdQuest = input("Which door would you like to chose? ")
+
+if (thirdQuest == "7"):
+  print("Welcome to door 7")
+  ques = input("Who is your favourite genre of music{Rap/HipHop, RandB, Country}: ")
+
+  if (ques == "Rap/HipHop"):
+    print("You like up beat music that you can jam with your friends")
+
+  if (ques == "RandB"):
+    print("You like smooth and relaxing music. You also like to just vibe a lot")
+
+  if (ques == "Country"):
+    print("You are a little old school and you like to enjoy long car rides with your windows down ")
+
+if (thirdQuest == "8"):
+  print("Welcome to door 8")
+  ques = input("What is your favourite shoe brand{Adidas, Jordan, Nike}: ")
+
+  if (ques == "Adidas"):
+    print("You are athletic and like sports, especially soccer or you just like how Adidas shoes look")
+
+  if (ques == "Jordan"):
+    print("You are an athletic person who probably prefers basketball over any sport")
+
+  if (ques == "Nike"):
+    print("You probably play soccer and try a lot of skill moves. You also might play American Football and just like Nike's design. ")
+
+if (thirdQuest == "9"):
+  print("Welcome to door 9")
+  ques = input("Where would you live if you had the choise{Barcelona, Paris, Los Angles}: ")
+
+  if (ques == "Barcelona"):
+    print("You probably like a lot of Spanish things")
+
+  if (ques == "Paris"):
+    print("You probably like the french culture and croissants")
+
+  if (ques == "Los Angles"):
+    print("You probably like a lot of famous people such as musicians and actors and you want to experience how it feels living around them")
+
+
+
+
+
